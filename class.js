d9d27664c4f43ce7e96ca64beb29672cfb7708fa
uzimaru <uzimaru0601@gmail.com>
2017-04-26 15:39:44 +0900

    ColorクラスのtoStringメソッドで上限、下限を設定

--- class.js
+++ class.js
@@ -517,6 +517,10 @@ class Color {
     }
 
     toString() {
+        this.r = this.r > 255 ? 255 : (this.r < 0 ? 0 : this.r);
+        this.g = this.g > 255 ? 255 : (this.g < 0 ? 0 : this.g);
+        this.b = this.b > 255 ? 255 : (this.b < 0 ? 0 : this.b);
+        this.a = this.a > 1 ? 1 : (this.a < 0 ? 0 : this.a);
         return 'rgba(' + this.r + ',' + this.g + ',' + this.b + ',' + this.a + ')';
     }
 
